d12d9037b8c9f70e4445580d5b3a2049fca51664
vamsi <vammu920@gmail.com>
2019-09-25 00:02:15 +0530

    Initial commit

---
+++ graphs.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include <stdlib.h>
+struct node
+{
+	int data;
+	struct node *left, *right;
+};
+struct node *newNode(int data){
+	struct node* node=(struct node*)malloc(sizeof(struct node));
+	node->data=data;
+	
+	node->right=node->left=NULL;
+	return node;
+
+};
+void search(stuct node* root, int data){
+	if(root==NULL || root->data==data) return printf("Found\n");
+
+	if(root->data<data) return search(root->right, data);
+	return search(root->left, data);
+}
+void inorder(struct node* root){
+	
+}
+void main(){
+	struct node* root=newNode(1);
+	root->left=newNode(2);
+	root->right=newNode(3);
+
+}
+
